9ec77dd62a3011afab0a339911b524a21542a57e
Jahed <jahed.public@gmail.com>
2018-07-28 17:39:48 +0100

    refactor: remove extraneous type

--- src/compose.ts
+++ src/compose.ts
@@ -5,7 +5,7 @@
  * Unlike `waterfall` which passes resolved values, you can use this to combine
  * other Promise consuming functions like `branch`.
  */
-export function compose (...steps: ((promise: Promise<any>) => Promise<any> | any)[]) {
+export function compose (...steps: ((promise: Promise<any>) => any)[]) {
   return (initial: any) => steps.reduce(
     (acc, next) => acc.then(
       accResult => next(Promise.resolve(accResult)),
