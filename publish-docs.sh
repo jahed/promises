dfb53c85b55eb9784e347c1332327853e14c493b
Jahed <jahed.public@gmail.com>
2018-07-28 16:02:28 +0100

    docs: build docs into a single page
    typedoc generates duplicate resources that aren't used.

--- publish-docs.sh
+++ publish-docs.sh
@@ -8,7 +8,7 @@ touch .nojekyll
 git init
 git remote add origin git@github.com:jahed/promises.git
 git checkout -b gh-pages
-git add .
+git add index.html
 git commit -m 'docs: new build'
 git push -u origin gh-pages --force
 
