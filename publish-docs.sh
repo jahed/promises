ea5a18a1986da126172f653b14aab7516222a0f3
Jahed <jahed.public@gmail.com>
2018-07-28 16:14:48 +0100

    chore: go back to using typedoc's default theme
    The minimal theme still requires loading a different page for links
    which makes it pointless and ironically bloated.

--- publish-docs.sh
+++ publish-docs.sh
@@ -8,7 +8,7 @@ touch .nojekyll
 git init
 git remote add origin git@github.com:jahed/promises.git
 git checkout -b gh-pages
-git add index.html
+git add .
 git commit -m 'docs: new build'
 git push -u origin gh-pages --force
 
